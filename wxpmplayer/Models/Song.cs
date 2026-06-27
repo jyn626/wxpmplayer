c1e3ec08e22c9486569682360287c6bca0448bb9
jyn626 <tuyor.jynn@gmail.com>
2026-06-27 09:24:10 +0800

    Revert "rename project to serein (1)"
    This reverts commit 66350b86b4031807c0a05d685a9de9d816658527.

---
+++ wxpmplayer/Models/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace wxpmplayer.Models
+{
+    public class Song
+    {
+        public string Title { get; set; } = "";
+        public string FilePath { get; set; } = "";
+        public string Artist { get; set; } = "";
+
+        public TimeSpan Duration { get; set; }
+        public BitmapImage AlbumImage { get; set; }
+    }
+}
---
+++ wxpmplayer/Services/PlaylistLoader.cs
@@ -0,0 +1,30 @@
+﻿using wxpmplayer.Models;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using TagLib; 
+
+namespace wxpmplayer.Services
+{
+    public class PlaylistLoader
+    {
+
+        public static List<Song> LoadSongs(string folder)
+        {
+           
+            string[] files = Directory.GetFiles(folder, "*.mp3");
+            List<Song> songs = new();
+
+            foreach (string file in files)
+            {
+                Song song = MetadataService.ReadMetadata(file);
+                songs.Add(song);
+            }
+
+
+            return songs;
+        }
+    }
+
+}
